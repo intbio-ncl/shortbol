dabb904a47dc14f4ff8cf0e6676f0f258f08c2d8
MattyCrowther <mattycrowther1@hotmail.co.uk>
2020-03-04 16:33:41 +0000

    Work in progress, believe the bug is fixed though

--- rdfscript/core.py
+++ rdfscript/core.py
@@ -84,11 +84,17 @@ class Identifier(Node):
             self.prefixify(context)
 
         uri = Uri('')
-
+        ts = "".join([str(p) for p in self.parts])
         for i, part in enumerate(self.parts):
             try:
+                if isinstance(part,Uri) and i > 0:
+                    try:
+                        if context.prefix_for_uri(part) in str(part):
+                            continue
+                    except PrefixError:
+                        pass
                 uri = uri + part.evaluate(context)
-
+                
                 binding = context.lookup(uri)
                 if binding is not None and i == len(self) - 1 :
                     uri = binding
@@ -208,7 +214,8 @@ class Uri(Node):
         self.uri = self.uri + delimiter + other.uri
 
     def split(self):
-        return re.split('#|/|:', self.uri)
+        splited = re.split('#|\/|:', self.uri)
+        return splited
 
     def evaluate(self, context):
         return self
