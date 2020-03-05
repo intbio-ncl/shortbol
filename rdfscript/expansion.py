fba732daef8510da46a900e5718de166740b137b
MattyCrowther <mattycrowther1@hotmail.co.uk>
2020-03-05 10:48:39 +0000

    Revert "Udpate"
    This reverts commit f9bcae5ed037cb66ebeb302da2e4832a7c0e2641.

--- rdfscript/expansion.py
+++ rdfscript/expansion.py
@@ -18,7 +18,7 @@ class Expansion(Node):
             else:
                 self.args.append(Argument(arg, n, location))
 
-        self.args.insert(0, Argument(self.identifier, -1))
+        self.args.append(Argument(self.identifier, -1))
 
         self.extensions = []
         self.body = []
